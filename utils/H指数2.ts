17a17a5b2310d85c140da141d56369e3e45ffc9f
ycte <114587040+ycte@users.noreply.github.com>
2023-10-31 21:18:24 +0800

    feat(): BigInt (#1)

--- utils/H指数2.ts
+++ utils/H指数2.ts
@@ -26,5 +26,28 @@ function hIndex1(citations: number[]): number {
 }
  
 console.log(hIndex1([3,0,6,1,5]))
-console.log(hIndex1([1,3,1]))
+console.log(hIndex1([1, 3, 1]))
+
+function hIndex2(cs: number[]): number {
+  const check = function (cs: number[], x: number): boolean {
+      let cnt: number = 0;
+      for (let c of cs) {
+          if (c >= x) cnt++;
+      }
+      return cnt >= x;
+  }
+  const n = cs.length;
+  let l = 0, r = n;
+  while (l < r) {
+      const mid = Math.floor((l + r + 1) / 2);
+      if (check(cs, mid)) l = mid;
+      else r = mid - 1;
+  }
+  return r;
+};
+
+// 作者：宫水三叶
+// 链接：https://leetcode.cn/problems/h-index/solutions/2502896/gong-shui-san-xie-cong-po-ti-dao-zhu-bu-7sug6/
+// 来源：力扣（LeetCode）
+// 著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
 
